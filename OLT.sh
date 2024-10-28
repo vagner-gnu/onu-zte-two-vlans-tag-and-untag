6330e6f83651dab4f27ab78b34575e7525f139e2
vagner-gnu <34011952+vagner-gnu@users.noreply.github.com>
2024-10-28 11:05:32 -0300

    Create OLT.sh

---
+++ OLT.sh
@@ -0,0 +1,21 @@
+configure terminal
+int gpon_olt-1/2/16
+onu 19 type ZTE-F601 sn DB17463E4173
+!
+interface gpon_onu-1/2/16:19
+name cliente-vlan-two
+sn-bind enable sn
+tcont 4 profile DADOS100MB
+gemport 1 tcont 4
+!
+interface vport-1/2/16.19:1
+  service-port 1 user-vlan 1500 vlan 4000
+  service-port 2 user-vlan 4001 vlan 4001
+!
+pon-onu-mng gpon_onu-1/2/16:19
+  service 1 gemport 1 vlan 4000
+  service 2 gemport 1 vlan 4001
+  vlan port eth_0/1 mode hybrid def-vlan 4000
+  vlan port eth_0/1 vlan 4000-4001
+!
+exit
